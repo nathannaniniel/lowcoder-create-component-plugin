f82cbbdc71c2fc19d864c80fd0f958dcef4b39a1
Nathaniel <nathaniel.nicholas@centralmegakencana.com>
2024-02-29 13:58:47 +0700

    add stone parcels list component

---
+++ lowcoder-comp-stone-parcels-list/src/index.ts
@@ -0,0 +1,7 @@
+import StoneParcelsList from "./StoneParcelsList";
+import HillchartsComp from "./HillchartsComp";
+
+export default {
+  hillcharts: HillchartsComp,
+  stoneparcelslist: StoneParcelsList
+};
